c6fb923aebe84d7a13dfb64c7e0b7fff074dd7e7
MirellaRossi <rm97990@fiap.com.br>
2023-04-18 10:43:53 -0300

    Listagem de Array com MAP, espalhando Array com SPREAD e conversão de HTML collection em Array

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -1,8 +1,5 @@
 
 //Função para recuperação de elementos no html para serem trabalhados os dados 
-//Pegando o elemento pelo seu tipo:
-
-const liElement = document.getElementsByTagName("li");
 
 
 //for (let index = 0; index <liElement.length; index++) {
@@ -11,18 +8,46 @@ const liElement = document.getElementsByTagName("li");
 //}
 //console.log(liElement.length)
 
-//Declaração de Arrays 
+// //Declaração de Arrays 
+// let num1=[1,2,3,4,5];
+// let num2=[6,7,8,9,10];
+// let num3=[num1,num2];
+
+
+// console.log(`Array-1 ${num1}`);
+// console.log(`Array-2 ${num2}`);
+// console.log(`Array-3 ${num3}`);
+
+// //Listando um Array, Técnica especial
+// /*num3.forEach((itemDoArray)=>{
+//     console.log(`Array-3 sendo listado ${itemDoArray}`);
+//  })*/
+ 
+//  // Listando o Array com MAP
+// num3.map((numero,key)=>{
+
+//     console.log(`${(key+1)} - Itens do array - ${numero}`);
+//     numero.forEach((digitos)=>{
+//         console.log(digitos)
+//     })
+// })
+
+
 let num1=[1,2,3,4,5];
 let num2=[6,7,8,9,10];
-let num3=[num1,num2];
-
+//Epalhar o Array com SPREAD [...]
+let num3=[...num1,...num2];
 
 console.log(`Array-1 ${num1}`);
 console.log(`Array-2 ${num2}`);
 console.log(`Array-3 ${num3}`);
 
-//Listabdo um Array, Técnica especial
-num3.forEach((itemDoArray)=>{
-    console.log(`Array-3 sendo listado ${itemDoArray}`);
+num3.map((numero,key)=>{
+    console.log(`${(key+1)} - Itens do array - ${numero}`);
 })
-console.log(`Array-3 sendo listado ${num3}`);
+
+//Pegando o elemento pelo seu tipo:
+//Conversão deste elemento HTML para Array de forma indireta 
+
+const liElementHtml = document.getElementsByTagName("li");
+const liElementArray = [...liElementHtml]
